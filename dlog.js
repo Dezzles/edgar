d9013f1b3f5d1a281320909d7cd864bd4ccdb628
Dezzles <daniel@dezzles.com>
2017-01-29 13:20:18 +1100

    Fixed log location

--- dlog.js
+++ dlog.js
@@ -6,7 +6,7 @@ var log = bunyan.createLogger({
   streams: [
     {
       level: 'info',
-      path: '/logs/dezzles/edgar/info.log'
+      path: '/log/dezzles/edgar/info.log'
     },
     {
       level: 'info',
